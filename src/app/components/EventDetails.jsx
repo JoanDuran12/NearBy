351d08b256f313f70750dee138962c6b00b42260
Joan Duran <joanmanuel808@gmail.com>
2025-07-09 19:41:28 -0400

    feat: Users are now able to register and unregister events

--- src/app/components/EventDetails.jsx
+++ src/app/components/EventDetails.jsx
@@ -2,20 +2,27 @@
 
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import { useAuth } from "@/app/contexts/AuthContext";
 import Link from "next/link";
 import {
   IconArrowLeft,
   IconCalendarFilled,
   IconClock,
   IconMapPin,
+  IconDoorExit,
 } from "@tabler/icons-react";
 
 export default function EventDetailsPage() {
   const params = useParams();
-  const [event, setEvent] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const { currentUser } = useAuth(); // Auth
+  const [event, setEvent] = useState(null); // Store the event data
+  const [loading, setLoading] = useState(true); // Loading Screen
+  const [error, setError] = useState(null); // Error Screen
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false); // Pop Up Leave Event Confirmation
+  const [isRegistered, setIsRegistered] = useState(null); // User is registered?
+  const [isHost, setIsHost] = useState(false); // User is Host of the event
 
+  // Convert standard 24 time to 12H AM/PM
   function formatTimeToAMPM(timeStr) {
     // timeStr is "HH:MM:SS"
     const [hourStr, minute] = timeStr.split(":");
@@ -25,34 +32,109 @@ export default function EventDetailsPage() {
     return `${hour}:${minute} ${ampm}`;
   }
 
+  // Fetch event data and event registration status
   useEffect(() => {
-    async function fetchEvent() {
-      if (!params.id) return;
+    async function fetchData() {
+      if (!params.id || !currentUser?.uid) return;
 
       try {
         setLoading(true);
-        const res = await fetch(
+
+        // Fetch event data
+        const eventRes = await fetch(
           `http://localhost:5000/api/events/${params.id}`,
           {
             cache: "no-store",
           }
         );
 
-        if (!res.ok) {
+        if (!eventRes.ok) {
           throw new Error("Event not found");
         }
 
-        const eventData = await res.json();
+        const eventData = await eventRes.json();
         setEvent(eventData);
-      } catch (err) {
-        setError(err.message);
+
+        // Fetch registration status
+        const regRes = await fetch(
+          `http://localhost:5000/api/atendees/${currentUser.uid}/${params.id}`
+        );
+
+        if (regRes.status === 404) {
+          setIsRegistered(false);
+          setIsHost(false);
+        } else if (!regRes.ok) {
+          throw new Error("Failed to check registration");
+        } else {
+          const regData = await regRes.json();
+          setIsRegistered(true);
+          setIsHost(!!regData.isHost);
+        }
+      } catch (error) {
+        setError(error.message);
       } finally {
         setLoading(false);
       }
     }
 
-    fetchEvent();
+    fetchData();
   }, [params.id]);
+
+  // Register user to event
+  const registerToEvent = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`http://localhost:5000/api/atendees`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          isHost: false,
+          eventId: params.id,
+          firebaseUid: currentUser.uid,
+        }),
+      });
+
+      if (!response.ok) {
+        console.log(await response.json());
+        throw new Error("Something went wrong");
+      }
+
+      const data = await response.json();
+      setIsRegistered(true);
+      console.log(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  // Unregister user from the event
+  const leaveEvent = async (e) => {
+    e.preventDefault();
+    setIsDropdownOpen(false);
+
+    try {
+      const response = await fetch(
+        `http://localhost:5000/api/atendees/${currentUser.uid}/${params.id}`,
+        {
+          method: "DELETE",
+        }
+      );
+
+      if (!response.ok) {
+        console.log(await response.json());
+        throw new Error("Something went wrong");
+      }
+
+      const data = await response.json();
+      setIsRegistered(false);
+      console.log(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   if (loading) return <div>Loading event...</div>;
   if (error) return <div>Error: {error}</div>;
@@ -120,9 +202,23 @@ export default function EventDetailsPage() {
               <span className="font-semibold text-sm bg-blue-100 text-blue-700 px-2 py-1 rounded">
                 {event.category || "N/A"}
               </span>
-              <button className="font-semibold text-sm px-3 py-2 rounded-md bg-gray-300 hover:underline">
-                Register
-              </button>
+              {isRegistered ? (
+                <button
+                  onClick={() => {
+                    setIsDropdownOpen(true);
+                  }}
+                  className="font-semibold text-sm px-3 py-2 rounded-md bg-red-500 hover:underline"
+                >
+                  Leave Event
+                </button>
+              ) : (
+                <button
+                  onClick={registerToEvent}
+                  className="font-semibold text-sm px-3 py-2 rounded-md bg-gray-300 hover:underline"
+                >
+                  Register
+                </button>
+              )}
             </div>
           </div>
         </div>
@@ -133,6 +229,37 @@ export default function EventDetailsPage() {
           {event.description || "No description available."}
         </p>
       </div>
+
+      {/* Leave Event Confirmation Modal */}
+      {isDropdownOpen && (
+        <div className="fixed inset-0 flex items-center justify-center backdrop-blur-xs bg-opacity-50 z-50">
+          <div className="flex flex-col w-[500px] h-[300px] p-8 bg-white rounded-xl shadow-lg border border-gray-300 justify-center items-center">
+            <h1 className="mb-4 text-2xl font-semibold text-red-500">
+              Cancel Registrarion
+            </h1>
+            <p className="mb-8 text-center">
+              Click on the confirm button below to cancel your registration.
+            </p>
+            <div className="flex justify-center items-center gap-8">
+              <button
+                onClick={leaveEvent}
+                className="flex justify-center items-center font-semibold text-sm px-3 py-2.5 rounded-md bg-red-500 hover:underline"
+              >
+                <IconDoorExit stroke={2} />
+                Confirm
+              </button>
+              <button
+                onClick={() => {
+                  setIsDropdownOpen(false);
+                }}
+                className="font-semibold text-sm px-5 py-3 rounded-md bg-gray-300 hover:underline"
+              >
+                Dismiss
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
